ff4a21c2ca0d375bbdaf7144131d50a0c452317a
mkrej <mkrej>
2024-07-09 09:49:55 +0200

    cuda dbg

--- llms_from_scratch_mamba.py
+++ llms_from_scratch_mamba.py
@@ -14,19 +14,33 @@
 
 import os
 import torch
+import gc
 from transformers import AutoModelForCausalLM , AutoTokenizer
 
+gc.collect()
 
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
     torch.cuda.empty_cache()
+
+    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = "max_split_size_mb:50"
+
+    allocated = torch.cuda.memory_allocated() / 1024**2
+    reserved = torch.cuda.memory_reserved() / 1024**2
+    free_mem, total_mem = torch.cuda.mem_get_info()
+    free_percentage = 100 * free_mem // total_mem
+    free_mem = free_mem / 1024**2
+    total_mem = total_mem / 1024**2
+    print('\n')
+    print(f'allocated: {allocated:.2f} MB, reserved: {reserved:.2f} MB, free_mem: {free_mem:.2f} MB, total_mem: {total_mem:.2f} MB, free_percentage: {free_percentage}% ')
+    print('\n')
+
 else:
     device = torch.device('cpu')
 print(f"device: {device}")
 
 model = AutoModelForCausalLM.from_pretrained('Q-bert/Mamba-1B', trust_remote_code=True).to(device)
 tokenizer = AutoTokenizer.from_pretrained('Q-bert/Mamba-1B')
-
 
 text = "Hi"
 text = "Hi, what do jou know about Wikipedia?"
@@ -36,7 +50,21 @@ input_ids = tokenizer.encode(text, return_tensors="pt").to(device)
 tokenizer.convert_ids_to_tokens(input_ids[0])
 
 # oryginalnie 20, 200 - b dłuuugo
-output = model.generate(input_ids, max_length=100, num_beams=5, no_repeat_ngram_size=2)
+try:
+    output = model.generate(input_ids, max_length=100, num_beams=5, no_repeat_ngram_size=2)
+
+except Exception as e:
+    print(e)
+    allocated = torch.cuda.memory_allocated() / 1024**2
+    reserved = torch.cuda.memory_reserved() / 1024**2
+    free_mem, total_mem = torch.cuda.mem_get_info()
+    free_percentage = 100 * free_mem // total_mem
+    free_mem = free_mem / 1024**2
+    total_mem = total_mem / 1024**2
+    print('\n')
+    print(f'allocated: {allocated:.2f} MB, reserved: {reserved:.2f} MB, free_mem: {free_mem:.2f} MB, total_mem: {total_mem:.2f} MB, free_percentage: {free_percentage}% ')
+    print('\n')
+    exit()
 
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 print(generated_text)
